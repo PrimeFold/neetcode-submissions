0582bc35a7a3e74e4118a3535cead745c8739b22
Aditya Raj <86973850+PrimeFold@users.noreply.github.com>
2026-06-15 22:36:54 +0530

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+       for(int[] row : matrix){
+            int l = 0 ;
+            int r = row.length - 1;
+            while(l<=r){
+                int mid = (l+r)/2;
+                if(row[mid]==target){
+                    return true;
+                }else if(row[mid]<target){
+                    l = mid + 1;
+                }else{
+                    r = mid - 1; 
+                }
+            }
+       }    
+        return false;
+    }
+}
